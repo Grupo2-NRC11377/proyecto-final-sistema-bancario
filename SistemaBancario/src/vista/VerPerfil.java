d2d07cc01a9682ab54e191f6213f820b332e5e9b
Jherson-Cahuana <n00327161@upn.pe>
2025-04-30 02:01:58 -0500

    Commit Jherson

--- SistemaBancario/src/vista/VerPerfil.java
+++ SistemaBancario/src/vista/VerPerfil.java
@@ -9,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
 
 public class VerPerfil extends JDialog {
 
@@ -38,62 +40,75 @@ public class VerPerfil extends JDialog {
 	public VerPerfil() {
 		setBounds(100, 100, 479, 571);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(new Color(255, 255, 255));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
 			JLabel lblNewLabel = new JLabel("Mi Perfil");
-			lblNewLabel.setBounds(195, 37, 46, 14);
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lblNewLabel.setBounds(185, 32, 105, 22);
 			contentPanel.add(lblNewLabel);
 		}
 		{
 			JLabel lblNewLabel_1 = new JLabel("Nombres y Apellidos");
-			lblNewLabel_1.setBounds(43, 100, 96, 14);
+			lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblNewLabel_1.setBounds(43, 95, 146, 22);
 			contentPanel.add(lblNewLabel_1);
 		}
 		{
 			JLabel lblNewLabel_2 = new JLabel("Telefono");
-			lblNewLabel_2.setBounds(43, 149, 46, 14);
+			lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblNewLabel_2.setBounds(43, 152, 83, 14);
 			contentPanel.add(lblNewLabel_2);
 		}
 		{
 			JLabel lblNewLabel_3 = new JLabel("Direccion");
-			lblNewLabel_3.setBounds(43, 206, 46, 14);
+			lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblNewLabel_3.setBounds(43, 209, 83, 14);
 			contentPanel.add(lblNewLabel_3);
 		}
 		{
-			JLabel lblNewLabel_4 = new JLabel("New label");
-			lblNewLabel_4.setBounds(43, 262, 46, 14);
+			JLabel lblNewLabel_4 = new JLabel("Correo");
+			lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			lblNewLabel_4.setBounds(43, 265, 83, 14);
 			contentPanel.add(lblNewLabel_4);
 		}
 		{
 			textField = new JTextField();
 			textField.setEditable(false);
-			textField.setBounds(238, 97, 86, 20);
+			textField.setBounds(219, 93, 171, 29);
 			contentPanel.add(textField);
 			textField.setColumns(10);
 		}
 		{
 			textField_1 = new JTextField();
 			textField_1.setEditable(false);
-			textField_1.setBounds(238, 149, 86, 20);
+			textField_1.setBounds(219, 146, 171, 29);
 			contentPanel.add(textField_1);
 			textField_1.setColumns(10);
 		}
 		{
 			textField_2 = new JTextField();
 			textField_2.setEditable(false);
-			textField_2.setBounds(238, 203, 86, 20);
+			textField_2.setBounds(219, 203, 171, 29);
 			contentPanel.add(textField_2);
 			textField_2.setColumns(10);
 		}
 		{
 			textField_3 = new JTextField();
 			textField_3.setEditable(false);
-			textField_3.setBounds(238, 259, 86, 20);
+			textField_3.setBounds(219, 259, 171, 29);
 			contentPanel.add(textField_3);
 			textField_3.setColumns(10);
 		}
+		
+		JButton btnNewButton = new JButton("Salir");
+		btnNewButton.setBounds(287, 393, 103, 36);
+		contentPanel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Regresar");
+		btnNewButton_1.setBounds(43, 393, 105, 36);
+		contentPanel.add(btnNewButton_1);
 	}
-
 }
