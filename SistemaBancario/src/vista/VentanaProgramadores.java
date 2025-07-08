892bc3df615ceb82335f9bc2299a9ea9f5bb6c4c
Gianella Basilio <n00396701@upn.pe>
2025-07-07 21:51:11 -0500

    Agregar foto

--- SistemaBancario/src/vista/VentanaProgramadores.java
+++ SistemaBancario/src/vista/VentanaProgramadores.java
@@ -8,32 +8,33 @@ import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class VentanaProgramadores extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JLabel lblProgramadores;
 	private JButton btnCerrar;
-	private JLabel lblNewLabel;
-	private JLabel lblFoto;
-	private JLabel lblFoto_2;
-	private JLabel lblFoto_1;
-	private JLabel lblFoto_3;
+	private JLabel lblFoto2;
+	private JLabel lblFoto3;
+	private JLabel lblFoto4;
+	private JLabel lblFoto5;
 	private JLabel lblBasilioAlvarez;
 	private JLabel lblGianellaAnnie;
-	private JLabel lblBasilioAlvarez_2;
+	private JLabel lblN00396701;
 	private JLabel lblCahuanaMoquillaza;
 	private JLabel lblHerbertJherson;
-	private JLabel lblBasilioAlvarez_3;
-	private JLabel lblBasilioAlvarez_4;
-	private JLabel lblGianellaAnnie_2;
-	private JLabel lblBasilioAlvarez_5;
-	private JLabel lblBasilioAlvarez_6;
-	private JLabel lblGianellaAnnie_3;
-	private JLabel lblBasilioAlvarez_7;
-	private JLabel lblBasilioAlvarez_8;
-	private JLabel lblGianellaAnnie_4;
-	private JLabel lblBasilioAlvarez_9;
+	private JLabel lblN00327161;
+	private JLabel lblCorcueraParedes;
+	private JLabel lblJoseManuel;
+	private JLabel lblN00373084;
+	private JLabel lblCotrinaSandoval;
+	private JLabel lblAlexandraMargoth;
+	private JLabel lblN00328935;
+	private JLabel lblDeLaCruzVilluga;
+	private JLabel lblKevinAndre;
+	private JLabel lblN00368206;
+	private JLabel lblFoto1;
 	
 	public VentanaProgramadores() {
 		setModal(true);
@@ -59,39 +60,32 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(btnCerrar);
 		}
 		{
-			lblNewLabel = new JLabel("Foto 1");
-			lblNewLabel.setForeground(new Color(0, 0, 0));
-			lblNewLabel.setBackground(new Color(255, 255, 255));
-			lblNewLabel.setBounds(50, 120, 137, 171);
-			getContentPane().add(lblNewLabel);
+			lblFoto2 = new JLabel("Foto 2");
+			lblFoto2.setForeground(new Color(0, 0, 0));
+			lblFoto2.setBackground(new Color(255, 255, 255));
+			lblFoto2.setBounds(210, 120, 137, 171);
+			getContentPane().add(lblFoto2);
 		}
 		{
-			lblFoto = new JLabel("Foto 2");
-			lblFoto.setForeground(new Color(0, 0, 0));
-			lblFoto.setBackground(new Color(255, 255, 255));
-			lblFoto.setBounds(210, 120, 137, 171);
-			getContentPane().add(lblFoto);
+			lblFoto3 = new JLabel("Foto 3");
+			lblFoto3.setForeground(new Color(0, 0, 0));
+			lblFoto3.setBackground(new Color(255, 255, 255));
+			lblFoto3.setBounds(370, 120, 137, 171);
+			getContentPane().add(lblFoto3);
 		}
 		{
-			lblFoto_2 = new JLabel("Foto 3");
-			lblFoto_2.setForeground(new Color(0, 0, 0));
-			lblFoto_2.setBackground(new Color(255, 255, 255));
-			lblFoto_2.setBounds(370, 120, 137, 171);
-			getContentPane().add(lblFoto_2);
+			lblFoto4 = new JLabel("Foto 4");
+			lblFoto4.setForeground(new Color(0, 0, 0));
+			lblFoto4.setBackground(new Color(255, 255, 255));
+			lblFoto4.setBounds(530, 120, 137, 171);
+			getContentPane().add(lblFoto4);
 		}
 		{
-			lblFoto_1 = new JLabel("Foto 3");
-			lblFoto_1.setForeground(new Color(0, 0, 0));
-			lblFoto_1.setBackground(new Color(255, 255, 255));
-			lblFoto_1.setBounds(530, 120, 137, 171);
-			getContentPane().add(lblFoto_1);
-		}
-		{
-			lblFoto_3 = new JLabel("Foto 3");
-			lblFoto_3.setForeground(new Color(0, 0, 0));
-			lblFoto_3.setBackground(new Color(255, 255, 255));
-			lblFoto_3.setBounds(690, 120, 137, 171);
-			getContentPane().add(lblFoto_3);
+			lblFoto5 = new JLabel("Foto 5");
+			lblFoto5.setForeground(new Color(0, 0, 0));
+			lblFoto5.setBackground(new Color(255, 255, 255));
+			lblFoto5.setBounds(690, 120, 137, 171);
+			getContentPane().add(lblFoto5);
 		}
 		{
 			lblBasilioAlvarez = new JLabel("Basilio Alvarez");
@@ -110,12 +104,12 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblGianellaAnnie);
 		}
 		{
-			lblBasilioAlvarez_2 = new JLabel("(N00396701)");
-			lblBasilioAlvarez_2.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_2.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_2.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_2.setBounds(50, 368, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_2);
+			lblN00396701 = new JLabel("(N00396701)");
+			lblN00396701.setHorizontalAlignment(SwingConstants.CENTER);
+			lblN00396701.setForeground(new Color(90, 90, 90));
+			lblN00396701.setFont(new Font("Arial", Font.BOLD, 15));
+			lblN00396701.setBounds(50, 368, 137, 18);
+			getContentPane().add(lblN00396701);
 		}
 		{
 			lblCahuanaMoquillaza = new JLabel("Cahuana Moquillaza");
@@ -134,84 +128,92 @@ public class VentanaProgramadores extends JDialog implements ActionListener {
 			getContentPane().add(lblHerbertJherson);
 		}
 		{
-			lblBasilioAlvarez_3 = new JLabel("(N00327161)");
-			lblBasilioAlvarez_3.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_3.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_3.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_3.setBounds(210, 368, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_3);
+			lblN00327161 = new JLabel("(N00327161)");
+			lblN00327161.setHorizontalAlignment(SwingConstants.CENTER);
+			lblN00327161.setForeground(new Color(90, 90, 90));
+			lblN00327161.setFont(new Font("Arial", Font.BOLD, 15));
+			lblN00327161.setBounds(210, 368, 137, 18);
+			getContentPane().add(lblN00327161);
 		}
 		{
-			lblBasilioAlvarez_4 = new JLabel("Corcuera Paredes");
-			lblBasilioAlvarez_4.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_4.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_4.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_4.setBounds(370, 310, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_4);
+			lblCorcueraParedes = new JLabel("Corcuera Paredes");
+			lblCorcueraParedes.setHorizontalAlignment(SwingConstants.CENTER);
+			lblCorcueraParedes.setForeground(new Color(90, 90, 90));
+			lblCorcueraParedes.setFont(new Font("Arial", Font.BOLD, 15));
+			lblCorcueraParedes.setBounds(370, 310, 137, 18);
+			getContentPane().add(lblCorcueraParedes);
 		}
 		{
-			lblGianellaAnnie_2 = new JLabel("Jose Manuel");
-			lblGianellaAnnie_2.setHorizontalAlignment(SwingConstants.CENTER);
-			lblGianellaAnnie_2.setForeground(new Color(90, 90, 90));
-			lblGianellaAnnie_2.setFont(new Font("Arial", Font.BOLD, 15));
-			lblGianellaAnnie_2.setBounds(370, 339, 137, 18);
-			getContentPane().add(lblGianellaAnnie_2);
+			lblJoseManuel = new JLabel("Jose Manuel");
+			lblJoseManuel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblJoseManuel.setForeground(new Color(90, 90, 90));
+			lblJoseManuel.setFont(new Font("Arial", Font.BOLD, 15));
+			lblJoseManuel.setBounds(370, 339, 137, 18);
+			getContentPane().add(lblJoseManuel);
 		}
 		{
-			lblBasilioAlvarez_5 = new JLabel("(N00373084)");
-			lblBasilioAlvarez_5.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_5.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_5.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_5.setBounds(370, 368, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_5);
+			lblN00373084 = new JLabel("(N00373084)");
+			lblN00373084.setHorizontalAlignment(SwingConstants.CENTER);
+			lblN00373084.setForeground(new Color(90, 90, 90));
+			lblN00373084.setFont(new Font("Arial", Font.BOLD, 15));
+			lblN00373084.setBounds(370, 368, 137, 18);
+			getContentPane().add(lblN00373084);
 		}
 		{
-			lblBasilioAlvarez_6 = new JLabel("Cotrina Sandoval");
-			lblBasilioAlvarez_6.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_6.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_6.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_6.setBounds(530, 310, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_6);
+			lblCotrinaSandoval = new JLabel("Cotrina Sandoval");
+			lblCotrinaSandoval.setHorizontalAlignment(SwingConstants.CENTER);
+			lblCotrinaSandoval.setForeground(new Color(90, 90, 90));
+			lblCotrinaSandoval.setFont(new Font("Arial", Font.BOLD, 15));
+			lblCotrinaSandoval.setBounds(530, 310, 137, 18);
+			getContentPane().add(lblCotrinaSandoval);
 		}
 		{
-			lblGianellaAnnie_3 = new JLabel("Alexandra Margoth");
-			lblGianellaAnnie_3.setHorizontalAlignment(SwingConstants.CENTER);
-			lblGianellaAnnie_3.setForeground(new Color(90, 90, 90));
-			lblGianellaAnnie_3.setFont(new Font("Arial", Font.BOLD, 15));
-			lblGianellaAnnie_3.setBounds(530, 339, 137, 18);
-			getContentPane().add(lblGianellaAnnie_3);
+			lblAlexandraMargoth = new JLabel("Alexandra Margoth");
+			lblAlexandraMargoth.setHorizontalAlignment(SwingConstants.CENTER);
+			lblAlexandraMargoth.setForeground(new Color(90, 90, 90));
+			lblAlexandraMargoth.setFont(new Font("Arial", Font.BOLD, 15));
+			lblAlexandraMargoth.setBounds(530, 339, 137, 18);
+			getContentPane().add(lblAlexandraMargoth);
 		}
 		{
-			lblBasilioAlvarez_7 = new JLabel("(N00328935)");
-			lblBasilioAlvarez_7.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_7.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_7.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_7.setBounds(530, 368, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_7);
+			lblN00328935 = new JLabel("(N00328935)");
+			lblN00328935.setHorizontalAlignment(SwingConstants.CENTER);
+			lblN00328935.setForeground(new Color(90, 90, 90));
+			lblN00328935.setFont(new Font("Arial", Font.BOLD, 15));
+			lblN00328935.setBounds(530, 368, 137, 18);
+			getContentPane().add(lblN00328935);
 		}
 		{
-			lblBasilioAlvarez_8 = new JLabel("De La Cruz Villuga");
-			lblBasilioAlvarez_8.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_8.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_8.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_8.setBounds(690, 310, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_8);
+			lblDeLaCruzVilluga = new JLabel("De La Cruz Villuga");
+			lblDeLaCruzVilluga.setHorizontalAlignment(SwingConstants.CENTER);
+			lblDeLaCruzVilluga.setForeground(new Color(90, 90, 90));
+			lblDeLaCruzVilluga.setFont(new Font("Arial", Font.BOLD, 15));
+			lblDeLaCruzVilluga.setBounds(690, 310, 137, 18);
+			getContentPane().add(lblDeLaCruzVilluga);
 		}
 		{
-			lblGianellaAnnie_4 = new JLabel("Kevin Andre");
-			lblGianellaAnnie_4.setHorizontalAlignment(SwingConstants.CENTER);
-			lblGianellaAnnie_4.setForeground(new Color(90, 90, 90));
-			lblGianellaAnnie_4.setFont(new Font("Arial", Font.BOLD, 15));
-			lblGianellaAnnie_4.setBounds(690, 339, 137, 18);
-			getContentPane().add(lblGianellaAnnie_4);
+			lblKevinAndre = new JLabel("Kevin Andre");
+			lblKevinAndre.setHorizontalAlignment(SwingConstants.CENTER);
+			lblKevinAndre.setForeground(new Color(90, 90, 90));
+			lblKevinAndre.setFont(new Font("Arial", Font.BOLD, 15));
+			lblKevinAndre.setBounds(690, 339, 137, 18);
+			getContentPane().add(lblKevinAndre);
 		}
 		{
-			lblBasilioAlvarez_9 = new JLabel("(N00368206)");
-			lblBasilioAlvarez_9.setHorizontalAlignment(SwingConstants.CENTER);
-			lblBasilioAlvarez_9.setForeground(new Color(90, 90, 90));
-			lblBasilioAlvarez_9.setFont(new Font("Arial", Font.BOLD, 15));
-			lblBasilioAlvarez_9.setBounds(690, 368, 137, 18);
-			getContentPane().add(lblBasilioAlvarez_9);
+			lblN00368206 = new JLabel("(N00368206)");
+			lblN00368206.setHorizontalAlignment(SwingConstants.CENTER);
+			lblN00368206.setForeground(new Color(90, 90, 90));
+			lblN00368206.setFont(new Font("Arial", Font.BOLD, 15));
+			lblN00368206.setBounds(690, 368, 137, 18);
+			getContentPane().add(lblN00368206);
+		}
+		{
+			lblFoto1 = new JLabel("");
+			lblFoto1.setIcon(new ImageIcon(VentanaProgramadores.class.getResource("/imagen/fotoCarnet_BasilioAlvarez.jpeg")));
+			lblFoto1.setForeground(Color.BLACK);
+			lblFoto1.setBackground(Color.WHITE);
+			lblFoto1.setBounds(50, 120, 137, 171);
+			getContentPane().add(lblFoto1);
 		}
 		setTitle("Programadores");
 		setBounds(100, 100, 900, 560);
